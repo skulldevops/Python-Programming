32613e0ad4b5c831ab890a6e199c9519adba6b5b
Rose Parker <145732598+skulldevops@users.noreply.github.com>
2024-09-05 09:19:16 -0400

    Add files via upload

--- startme.py
+++ startme.py
@@ -1,3 +1,3 @@
 import os
 # Welcome to my Python Projects when you run this file, it will open (1) of my Python Projects I've created in VSC
-os.system("open smoothie.py")
+os.system("open calc.py")
