1da8ea0dca09e34bae1d104921be65bce29b5c04
Rose Parker <145732598+skulldevops@users.noreply.github.com>
2024-10-03 22:18:36 -0400

    Add files via upload

---
+++ clicker.py
@@ -0,0 +1,22 @@
+#Welcoome too a simple clicker, I start with importing tkinter for our app
+import tkinter
+root = tkinter.Tk()
+#I add a title and name it
+root.title("Just a Clicker")
+#I set our window dimensions
+root.geometry("200x65")
+#I set num as 0 for our click counter to update {num}
+num = 0
+#I add a label to display the number of clicks with the push of a button
+label = tkinter.Label(root, text="Clicks: 0")
+label.pack()
+#I create a function that counts and grabs the {num} of clicks using global
+def count_clicks():
+    global num
+    num += 1
+    label.config(text=f"Clicks: {num}")
+#I create a button that prompts the user to click the button, which will update
+#the clicks every time it's pressed
+button = tkinter.Button(root, text="Click", command=count_clicks)
+button.pack()
+root.mainloop()
