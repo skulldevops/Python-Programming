24b9836e4e572ec6e45a8955b2de11d15918334d
Rose Parker <145732598+skulldevops@users.noreply.github.com>
2024-10-13 14:07:42 -0400

    Add files via upload
    Added color to the button. Simplified the clicker label

--- clicker.py
+++ clicker.py
@@ -8,15 +8,17 @@ root.geometry("200x65")
 #I set num as 0 for our click counter to update {num}
 num = 0
 #I add a label to display the number of clicks with the push of a button
-label = tkinter.Label(root, text="Clicks: 0")
+label = tkinter.Label(root, text="0")
 label.pack()
 #I create a function that counts and grabs the {num} of clicks using global
 def count_clicks():
     global num
     num += 1
-    label.config(text=f"Clicks: {num}")
+    label.config(text=f"{num}")
+
 #I create a button that prompts the user to click the button, which will update
 #the clicks every time it's pressed
 button = tkinter.Button(root, text="Click", command=count_clicks)
+button.config(foreground="red")
 button.pack()
 root.mainloop()
