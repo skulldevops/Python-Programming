a16317f9ddca3d5e13f9777a9044d174d88747ed
Rose Parker <145732598+skulldevops@users.noreply.github.com>
2024-10-27 23:54:01 -0400

    Add files via upload

--- getfile.py
+++ getfile.py
@@ -1,22 +1,28 @@
+#I import tkinter to create ooutr window
 import tkinter as tk
+#I import zipfile too get oour zip file ready for download
 import zipfile
 from tkinter import filedialog
 from tkinter import *
+#I create the parameters for our window using Root tk.Tk()
 root = tk.Tk()
+#I add a simple title foor this window
 root.title("Skull Dev-Ops")
-
+#I create a function to get the file using filedialog and askopenfilename from my computer
 def download_files():
     files = []
     file1 = filedialog.askopenfilename(initialdir="/Users/user/Desktop/Portfolio/Fusion.zip", filetypes="Text Files" ["*.zip"])
     zip_filename = "Fusion.zip"
     zip_file = zipfile.ZipFile(zip_filename, "w")
 
+#I create a label for readability so you know what you're downloading
 label = tk.Label(root, text="Download Fusion Project Pack>>>")
 label.config(foreground="green")
 label.pack(side="left")
 
+#I add a button so when the user clicks the button can be ready for download
 download = tk.Button(root, text="Download")
 download.config(foreground="green")
 download.pack(side="right")
-
+#Never foroget the root.mainloop() too close our parameters for this window
 root.mainloop()
--- weather.py
+++ weather.py
@@ -1,13 +1,18 @@
+# I start with importing Tkinter for our Python window
 import tkinter as tk
+#I import requests to get API Keys for our Weather App
 import requests
+#I import Java Script for our weather app
 import _json
 
+#First I add root window as Tk
 root = tk.Tk()
+#I add a title for our window
 root.title("Weather")
-
+#I create a label for the page we created named Local Weather
 page_label = tk.Label(root, text="Your Local Weather Forecast")
 page_label.pack()
-
+#Here is the fuction I created to get the weather api key from the users local area
 def getWeatherData(location):
     api_key = ""
     url = "url of the API>"
@@ -16,5 +21,5 @@ def getWeatherData(location):
     temperature = data["temperature"]
     humidity = data["humidity"]
     weather_forecast = data["weather_forecast"]
-
+#I make sure to close out our window with root.mainloop()
 root.mainloop()
