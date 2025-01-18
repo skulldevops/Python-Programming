450155f297826a2dbc9d20dd82550741b8a9fce3
Rose Parker <145732598+skulldevops@users.noreply.github.com>
2025-01-17 17:27:27 -0800

    Add files via upload

--- test.py
+++ test.py
@@ -2,41 +2,96 @@ import tkinter
 
 root = tkinter.Tk()
 root.title("Function Test")
-root.geometry("200x150")
+root.geometry("300x300")
 
-def change():
+name = ["Mario, Luigi, Wario"]
+grocery = ["Bananas", "Oranges", "Apples"]
+
+a_value = 0
+b_value = 0
+result = 0
+
+#Update Function
+def update():
     if clicks:
-        print("Updated...")
-        return click_count.config(text="Updated...")
+        print("Updating...")
+        print("Done!")
+        return click_count.config(text="Updated!")
     
-def update():        
-    while user_input.get():
+#Detect user typing Function 
+def change():        
+    if user_input.get():
         print("User typing...")
-     
-def grab():
-    while num_input.get() == int():   
-        get_num in num_input.get()
-        if get_num in num_input.get() == False:
-            print("That is not a valid number")
+    else: 
+        user_input.get() == None
+        print("No words detected")
+        
+#Add function       
+def on_click_plus():
+    a_value = float(entry1.get())
+    b_value = float(entry2.get())
+    result = a_value + b_value
+    entry3.insert(0, result)
+    
+def clear_entry3():
+    entry3.delete(0, tkinter.END)
+    
+#List Function
+def print_list():
+    if list_entry.get() == "name":
+        print(name)
+        return list_label.config(text="Name list printed!")
+    elif list_entry.get() == "grocery":
+        print(grocery)
+        return list_label.config(text="Grocery list printed!")
+    else:
+        user_input.get() == None
+        print("No list detected!")
+        return list_label.config(text="No list detected!")
+            
 #String test function
 click_count = tkinter.Label(root, text="String Test")
 click_count.pack()
 
-detect_typing = tkinter.Button(root, text="Detect User Typing...", command=update)
+detect_typing = tkinter.Button(root,text="Detect User Typing", command=change)
 detect_typing.pack()
  
 #Label Update function   
-clicks = tkinter.Button(root, text="Clicks", command=change)
+clicks = tkinter.Button(root, text="Update", command=update)
 clicks.pack()
 
 user_input = tkinter.Entry(root)
 user_input.pack()
 
-#Num detection function
-num_input = tkinter.Entry(root)
-num_input.pack()
+#Add Function
+entry1 = tkinter.Entry(root, textvariable=input)
+entry1.pack()
 
-get_num = tkinter.Button(root, text="Get Number", command=grab)
-get_num.pack()
+entry2 = tkinter.Entry(root, textvariable=input)
+entry2.pack()
+
+#Add Button and Clear button
+
+button = tkinter.Button(root, text="+", width=5)
+button.pack(side="left")
+button.bind(on_click_plus)
+button.config(command=on_click_plus)
+
+clear_button =tkinter.Button(root,width=3, text="Clear", command=lambda: clear_entry3())
+clear_button.bind(clear_entry3)
+clear_button.pack(side="left")
+
+entry3 = tkinter.Entry(root, textvariable=input)
+entry3.pack()
+
+#List Function
+list_entry = tkinter.Entry(root)
+list_entry.pack()
+
+list_label = tkinter.Label(root, text="___List___")
+list_label.pack()
+
+get_list = tkinter.Button(root, text="Print", command=print_list)
+get_list.pack()
 
 root.mainloop()
